c2428bfbba7b8dc947dc2709e9f777445601a8e5
John Russell <j362r647@ku.edu>
2016-10-24 15:43:40 -0500

    Created first version of multi-machine TCP socket programs

---
+++ TCPClientExperimental.py
@@ -0,0 +1,22 @@
+#TCPClientExperimental.py
+
+import socket
+
+import sys
+
+ipNum = sys.argv[1]
+
+portNum = int(sys.argv[2])
+
+message = raw_input("Message to send to server: ")
+
+sendingSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sendingSocket.connect((ipNum,portNum))
+
+while 1:	
+	sendingSocket.send(message)
+	data = sendingSocket.recv(1024)
+	print data
+	message = raw_input("Message to send to server: ")
+	
+s.close()
